7b3eedf09d6c7ee10742bd8be4faa0d678990906
jessica-calderon <calderonjessica13@yahoo.com>
2022-05-17 01:39:20 -0500

    prevent refresh on submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 // assign the button element objection representation to a variable
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // function to dynamically create the task item 
 var createTaskHandler = function() {
+    // prevent refresh on submit 
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-};
-// event listener that uses createTaskHandler as a callback function
-buttonEl.addEventListener("click", createTaskHandler);
+    };
+    
+    // event listener that uses createTaskHandler as a callback function
+    formEl.addEventListener("submit", createTaskHandler);
